e2aaebfdd486f6e20eab0a06a16a3e8b9145d66a
Mike Puzio <mcpuzio@gmail.com>
2015-12-14 20:43:18 -0500

    New additions

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 $(document).ready( function() {
 
-$('.lightbox-link').on("click", function() {
+$('.lightbox-link').on("click", function(e) {
+	e.preventDefault();
+	var $image = $('#this-image');
+	$image.attr("src", $(this).attr("src"));
 	$("#outer_lightbox").slideToggle();
 
 })
 $('#this-image').on("click", function() {
 	$('#outer_lightbox').hide();	
 })
+
+$('.previous').click(function(){
+        var nextImageLocation = $(this).next.attr('href');
+        $nextLightboxImage.attr('src', nextImageLocation);
+        $lightbox.detach($lightboxImage);
+        $lightbox.append($nextLightboxImage);
+    });
 });
